7e2fdce3c0802d118d94dd1548328f4a437af25e
Md Mahiuddin <68785084+mahiuddin-dev@users.noreply.github.com>
2022-04-11 16:15:36 +0600

    netlify site settings

--- UserInfo/settings.py
+++ UserInfo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djangoreact2022.herokuapp.com']
+ALLOWED_HOSTS = ['djangoreact2022.herokuapp.com','bespoke-nougat-375bc2.netlify.app']
 
 
 # Application definition
@@ -141,5 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "https://reactdjango2022.herokuapp.com",
     "http://reactdjango2022.herokuapp.com",
+    "https://bespoke-nougat-375bc2.netlify.app/",
+    "http://bespoke-nougat-375bc2.netlify.app/",
 ]
 django_heroku.settings(locals())
